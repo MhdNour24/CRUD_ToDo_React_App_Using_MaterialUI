9314863b82a7f25298573fa4fde9948401bc8d75
MHD NOUR <monuur2003@gmail.com>
2024-06-19 22:09:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import ToDoList from './components/ToDoList';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { TodosContext } from './contexts/todosContext';
+import {v4 as uuidv4} from 'uuid';
+
+const theme = createTheme({
+  typography:{
+    fontFamily:[
+      "alexandria"
+    ]
+  }
+});
+const initialTodos = [
+
+  {
+    id: uuidv4(),
+    title: "إنهاء المشروع",
+    details: "إكمال العرض التقديمي الخاص بمشروع الشركة وتسليمه قبل الموعد النهائي.",
+    isCompleted: false
+  },
+  {
+    id: uuidv4(),
+    title: "زيارة الطبيب",
+    details: "زيارة طبيب الأسنان للفحص الدوري وتنظيف الأسنان.",
+    isCompleted: false
+  },
+];
 
 function App() {
+  const [todos,setTodos]=React.useState(initialTodos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      
+      <div className="App"  style={{
+
+        backgroundColor:"#b0e0e6",
+        direction:"rtl",
+        minHeight:"100vh"
+        }} 
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <TodosContext.Provider value={{todos:todos,setTodos:setTodos}}>
+              <ToDoList></ToDoList>
+          </TodosContext.Provider>
+      </div>
+    </ThemeProvider>
   );
 }
 
 export default App;
+ 
