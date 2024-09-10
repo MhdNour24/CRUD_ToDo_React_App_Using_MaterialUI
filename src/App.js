f2a5f08b892c1437b4a3186ae29bcd31a809f3fd
MHD NOUR EDDIN BASSAL <119541895+MhdNour24@users.noreply.github.com>
2024-09-10 10:47:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import './App.css';
-import * as React from 'react';
-import ToDoList from './components/ToDoList';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import "./App.css";
+import * as React from "react";
+import ToDoList from "./components/ToDoList";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 // import {v4 as uuidv4} from 'uuid';
-import {ToastPorvider} from './contexts/ToastContext';
-import TodosProvider from './contexts/todosContext';
-
+import { ToastPorvider } from "./contexts/ToastContext";
+import TodosProvider from "./contexts/todosContext";
 
 const theme = createTheme({
-  typography:{
-    fontFamily:[
-      "alexandria"
-    ]
-  }
+  typography: {
+    fontFamily: ["alexandria"],
+  },
 });
 
+
 function App() {
-
-
   return (
     <ThemeProvider theme={theme}>
       <TodosProvider>
-        <ToastPorvider >
-          <div className="App"  style={{
-
-            backgroundColor:"#b0e0e6",
-            direction:"rtl",
-            minHeight:"100vh"
-            }} 
-            >
-                  <ToDoList></ToDoList>
+        <ToastPorvider>
+          <div
+            className="App"
+            style={{
+              backgroundColor: "#b0e0e6",
+              direction: "ltr",
+              minHeight: "100vh",
+            }}
+          >
+            <ToDoList></ToDoList>
           </div>
         </ToastPorvider>
       </TodosProvider>
@@ -38,4 +35,3 @@ function App() {
 }
 
 export default App;
- 
