26888b10d406de9ede814430a27de36caf2c897d
MHD NOUR EDDIN BASSAL <119541895+MhdNour24@users.noreply.github.com>
2024-09-10 10:48:31 +0300

    Update ToDo.jsx

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import Stack from '@mui/material/Stack';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -10,80 +10,100 @@ import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 import DeleteOutlineRoundedIcon from '@mui/icons-material/DeleteOutlineRounded';
 import ModeEditOutlineRoundedIcon from '@mui/icons-material/ModeEditOutlineRounded';
 import { ToastContext } from '../contexts/ToastContext';
-
 import { TodosContext } from '../contexts/todosContext';
-import { useContext } from 'react';
 
-
-
-function ToDo({todoInfo,showDelete,showUpdate}) {
-    const {todos,dispatch}=useContext(TodosContext);
-    const {showHideToast} =useContext(ToastContext)
+function ToDo({ todoInfo, showDelete, showUpdate }) {
+    const { todos, dispatch } = useContext(TodosContext);
+    const { showHideToast } = useContext(ToastContext);
 
     function handleCheckClick() {
-        dispatch({type:"checking",payload:{todoInfo}})
-        todoInfo.isCompleted? showHideToast("تم الاضافة الى المهمات المنجزة بنجاح"):showHideToast("تم الغاء الاضافة الى المهمات المنجزة")
+        dispatch({ type: "checking", payload: { todoInfo } });
+        todoInfo.isCompleted 
+            ? showHideToast("Task successfully marked as completed") 
+            : showHideToast("Task marked as not completed");
     }
 
-
-  return (
-    <>
-        <Card sx=
-        {{
-            transition:"all 0.3s ease",
-             minWidth: 275 ,
-             background:"#088da5",
-             color:"white",
-             marginTop:5,
-             "&:hover":{
-                backgroundColor: "#81d8d0", // تغيير لون الخلفية عند التمرير
-                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // إضافة ظل للبطاقة
-                paddingTop:1,
-                paddingBottom:1
-             }
-             }}>  
-            <CardContent>
-                <Box sx={{ flexGrow: 1 }} >
-                    <Grid container spacing={2}>
-                        <Grid xs={9} >
-                            <Typography  sx={{textAlign:"right",textDecoration:todoInfo.isCompleted?"line-through":"none"}} variant='h5' >{todoInfo.title}</Typography>
-                            <br />
-                            <Typography  sx={{textAlign:"right"}} variant='h6' >{todoInfo.details}</Typography>
+    return (
+        <>
+            <Card
+                sx={{
+                    transition: "all 0.3s ease",
+                    minWidth: 275,
+                    background: "#088da5",
+                    color: "white",
+                    marginTop: 5,
+                    "&:hover": {
+                        backgroundColor: "#81d8d0", // Change background color on hover
+                        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Add shadow to card
+                        paddingTop: 1,
+                        paddingBottom: 1
+                    }
+                }}
+            >
+                <CardContent>
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={2}>
+                            <Grid xs={9}>
+                                <Typography
+                                    sx={{
+                                        textAlign: "left",
+                                        textDecoration: todoInfo.isCompleted ? "line-through" : "none"
+                                    }}
+                                    variant='h5'
+                                >
+                                    {todoInfo.title}
+                                </Typography>
+                                <br />
+                                <Typography sx={{ textAlign: "left" }} variant='h6'>
+                                    {todoInfo.details}
+                                </Typography>
+                            </Grid>
+                            <Grid xs={3}>
+                                {/* Start action buttons */}
+                                <Stack
+                                    direction="row"
+                                    sx={{
+                                        display: "flex",
+                                        justifyContent: "space-between",
+                                        alignItems: "center",
+                                        flexWrap: "wrap"
+                                    }}
+                                >
+                                    <IconButton
+                                        sx={{
+                                            background: todoInfo.isCompleted ? "#8bc34a" : "white",
+                                            color: !todoInfo.isCompleted ? "#8bc34a" : "white",
+                                        }}
+                                        onClick={handleCheckClick}
+                                    >
+                                        <CheckCircleRoundedIcon />
+                                    </IconButton>
+                                    <IconButton
+                                        aria-label="delete"
+                                        sx={{ color: "#b23c17", background: "white" }}
+                                        onClick={() => {
+                                            showDelete(todoInfo);
+                                        }}
+                                    >
+                                        <DeleteOutlineRoundedIcon />
+                                    </IconButton>
+                                    <IconButton
+                                        sx={{ color: "#1769aa", background: "white" }}
+                                        onClick={() => {
+                                            showUpdate(todoInfo);
+                                        }}
+                                    >
+                                        <ModeEditOutlineRoundedIcon />
+                                    </IconButton>
+                                </Stack>
+                                {/* End action buttons */}
+                            </Grid>
                         </Grid>
-                        <Grid xs={3} >
-                            {/* start action buttons */}
-                            <Stack direction="row" sx={{display:"flex",justifyContent:"space-between",alignItems:"center",flexWrap:"wrap"}}>
-                                <IconButton  sx={{
-                                    background:todoInfo.isCompleted ? "#8bc34a" :"white",
-                                    color:! todoInfo.isCompleted ? "#8bc34a" :"white",}}
-                                    onClick={()=>{
-                                        handleCheckClick();
-                                    }}>
-                                    <CheckCircleRoundedIcon />
-                                </IconButton>
-                                <IconButton aria-label="delete"  sx={{color:"#b23c17",background:"white"}} onClick={()=>{
-                                    showDelete(todoInfo)
-                                }}>
-                                    <DeleteOutlineRoundedIcon />
-                                </IconButton>
-                                <IconButton sx={{color:"#1769aa",background:"white"}} onClick={()=>{
-                                    showUpdate(todoInfo)
-                                }}>
-                                    <ModeEditOutlineRoundedIcon />
-                                </IconButton> 
-                            </Stack>
-                            {/* end action buttons */}
-                        </Grid>
-                    </Grid>
-                </Box>                
-                {/* <Divider/> */}
-            </CardContent>
-
-        </Card>
-    </>
-  )
+                    </Box>
+                </CardContent>
+            </Card>
+        </>
+    );
 }
 
-export default ToDo
-
-
+export default ToDo;
